a01cd95025660fc26b41a42297f01525879b5076
Ammar Ahmed <ammarahmed8985@gmail.com>
2025-10-20 04:38:41 +0500

    Implemented Auth, Billing, Database Setup, Create Companion Server action

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import React from "react";
+import {ClerkProvider} from "@clerk/nextjs";
+import "./globals.css";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,11 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
+      <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
+          <Navbar />
+          {children}
+      </ClerkProvider>
       </body>
-    </html>
+      </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,50 @@
+import CompanionCard from "@/components/CompanionCard"
 import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionList from "@/components/CompaniansList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-        <h1 className="test-2x1 underline">Ammar Ahmed</h1>
-        <Button>
-            get started
-        </Button>
-    </div>
+    <main>
+        <h1 className="test-2x1 underline">Popular Companions</h1>
+
+        <section className="home-section">
+            <CompanionCard
+                id="123"
+                name="Neura the Brany Explorer"
+                topic="Neura Network of the Brain"
+                subject="Science"
+                duration={45}
+                color="#ffda6e"
+            />
+            <CompanionCard
+                id="456"
+                name="Countsy the Number Wizard"
+                topic="Derivative & Integrals"
+                subject="Maths"
+                duration={30}
+                color="#e5d0ff"
+            />
+            <CompanionCard
+                id="789"
+                name="Verval the Vocabulary Builder"
+                topic="Language"
+                subject="English Literature"
+                duration={30}
+                color="#BDE7FF"
+            />
+        </section>
+
+        <section className="home-section">
+            <CompanionList
+                title="Recently completed Session"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA />
+        </section>
+    </main>
   )
 }
 
